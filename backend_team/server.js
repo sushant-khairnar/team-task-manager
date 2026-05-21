6b8bf67ebfa0a6eb7f376df309d4580224672995
Sushant <sushantkhairnar27@gmail.com>
2026-05-21 12:49:06 +0530

    Fixed CORS

--- backend_team/server.js
+++ backend_team/server.js
@@ -5,7 +5,9 @@ const express=require("express");
 const cors=require("cors");
 
 const app=express();
-app.use(cors());
+app.use(cors({
+  origin: "*",
+}));
 app.use(express.json());
 app.get("/",(req,res)=>
 {
